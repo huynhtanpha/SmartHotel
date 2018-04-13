30e47595091e21385608a60d898807ad6eec7c95
Huynh Tan Pha <phahuynh@dicentral.vn>
2018-04-13 18:58:31 +0700

    Add project files.

---
+++ SmartHotel/SmartHotel/Views/LoginView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartHotel.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginView : ContentPage
+	{
+		public LoginView ()
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+            SignUp.Clicked += SignUp_Clicked;
+		}
+
+        private void SignUp_Clicked(object sender, EventArgs e)
+        {
+            if(UserNameEntry.Text == null || Password.Text == null)
+            {
+                DisplayAlert("Error", "Please, Input your user name or password", "OK");
+            }
+            if (UserNameEntry.Text == "phahuynh" && Password.Text == "123456")
+            {
+                //Chuyển qua trang mới
+                Navigation.PushAsync(new MainView());
+            }
+        }
+    }
+}
---
+++ SmartHotel/SmartHotel/Views/MainView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartHotel.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainView : ContentPage
+	{
+		public MainView ()
+		{
+			InitializeComponent ();
+            Back.Clicked += Back_Clicked;
+		}
+
+        private void Back_Clicked(object sender, EventArgs e)
+        {
+            //chuyển về trang trước đó
+            Navigation.PopAsync();
+        }
+    }
+}
